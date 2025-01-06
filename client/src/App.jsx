a92fbb2249361df37b244964f191c7db190447f1
pepe-le-pewploo <mamun.muntasir003@gmail.com>
2025-01-06 20:21:24 +0600

    Created Layout and Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,48 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+//import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import AuthLayout from './components/auth/AuthLayout'
+import AuthLogin from './pages/auth/Login'
+import AuthRegister from './pages/auth/Register'
+import AdminLayout from './components/admin-view/AdminLayout'
+import AdminDashboard from './pages/admin-view/AdminDashboard'
+import AdminProducts from './pages/admin-view/AdminProducts'
+import AdminFeatures from './pages/admin-view/AdminFeatures'
+import AdminOrders from './pages/admin-view/AdminOrders'
+import ShoppingLayout from './components/shopping-view/ShoppingLayout'
+import NotFound from './pages/not-found'
+import ShoppingAccount from './pages/shopping-view/ShoppingAccount'
+import ShoppingHome from './pages/shopping-view/ShoppingHome'
+import ShoppingCheckout from './pages/shopping-view/ShoppingCheckout'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='flex flex-col overflow-hidden bg-white'>
+      <h1>Ecommerece</h1>
+      <Routes>
+        <Route path='/auth' element={<AuthLayout/>}>
+          <Route path='login' element={<AuthLogin/>}/>
+          <Route path='register' element={<AuthRegister/>}/>
+        </Route>
+        <Route path='/admin' element={<AdminLayout/>}>
+          <Route path='dashboard' element={<AdminDashboard/>}/>
+          <Route path='products' element={<AdminProducts/>}/>
+          <Route path='features' element={<AdminFeatures/>}/>
+          <Route path='orders' element={<AdminOrders/>}/>
+        </Route>
+        <Route path='/shop' element={<ShoppingLayout/>}>
+          <Route path='account' element={<ShoppingAccount/>}/>
+          <Route path='home' element={<ShoppingHome/>}/>
+          <Route path='checkout' element={<ShoppingCheckout/>}/>
+        </Route>
+        <Route path='*' element={<NotFound/>}/>
+      </Routes>
+ 
+    </div>
   )
 }
 
---
+++ client/src/pages/auth/Register.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AuthRegister = () => {
+  return (
+    <div>Register</div>
+  )
+}
+
+export default AuthRegister
