c4bfe587ea9698e422ed1e3d439270249fd3f89b
pepe-le-pewploo <mamun.muntasir003@gmail.com>
2025-01-10 15:19:46 +0600

    db model, controllers,routes, etc. created for address page

--- server/index.js
+++ server/index.js
@@ -8,6 +8,9 @@ const authRouter = require('./routes/auth/auth-routes')
 const adminProductsRouter = require('./routes/admin/products-routes')
 const shopProductsRouter = require('./routes/shop/products-routes')
 const shopCartRouter = require("./routes/shop/cart-routes");
+const shopAddressRouter = require("./routes/shop/address-routes");
+
+
 app.use(express.json())
 app.use(
   cors({
@@ -28,6 +31,7 @@ app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
 app.use('/api/shop/products', shopProductsRouter);
 app.use('/api/shop/cart', shopCartRouter)
+app.use('/api/shop/address', shopAddressRouter);
 
 app.get('/', (req,res) => {
   res.send(`<h1>Home Page</h1>`)
